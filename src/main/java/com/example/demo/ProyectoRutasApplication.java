d90ef48482c82c957d33efdd4809c4008c1eecc5
Camilo-02 <camiloandreschavarroguenis@gmail.com>
2023-05-10 21:11:27 -0500

    commit 1.0

---
+++ src/main/java/com/example/demo/ProyectoRutasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRutasApplication.class, args);
+	}
+
+}

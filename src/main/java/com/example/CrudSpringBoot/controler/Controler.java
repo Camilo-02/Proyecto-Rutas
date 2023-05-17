4551d51b001dc81f9dca995f453c77b3e895f04c
Camilo-02 <camiloandreschavarroguenis@gmail.com>
2023-05-17 15:11:31 -0500

    nuevo nuevo

--- src/main/java/com/example/CrudSpringBoot/controler/Controler.java
+++ src/main/java/com/example/CrudSpringBoot/controler/Controler.java
@@ -1,4 +1,5 @@
 package com.example.CrudSpringBoot.controler;
+
 import com.example.CrudSpringBoot.interfaceService.IClienteService;
 import com.example.CrudSpringBoot.modelo.Cliente;
 
@@ -19,46 +20,57 @@ public class Controler {
     @Autowired
     private IClienteService service;
 
-@GetMapping("/inicio")
-public String agregar(Model model){
-    return "interface";
-}
-
-
-
+    @GetMapping("/inicio")
+    public String agregar(Model model) {
+        return "interface";
+    }
     @GetMapping("/listar")
-    public String listar(Model model){
-        List<Cliente>cliente= service.listar();
+    public String listar(Model model) {
+        List<Cliente> cliente = service.listar();
         model.addAttribute("cliente", cliente);
         return "index";
     }
 
+    @GetMapping("/rutaDos")
+    public String agregar2(Model model) {
+        return "rutaDos";
+    }
+    @GetMapping("/rutaTres")
+    public String agregar3(Model model) {
+        return "rutaTres";
+    }
+
+    @GetMapping("/rutaUno")
+    public String ver(Model model) {
+        return "rutaUno";
+    }
+
+
+
     @GetMapping("/VentaBoleto")
-    public String agregar1(Model model){
+    public String agregar1(Model model) {
         model.addAttribute("cliente", new Cliente());
         return "ventaBoleto";
     }
 
     @PostMapping("/save")
-    public String save(@Validated Cliente c){
+    public String save(@Validated Cliente c) {
         service.save(c);
         return "redirect:/inicio";
     }
 
     @GetMapping("/editar/{id}")
-    public String editar(@PathVariable int id, Model model){
+    public String editar(@PathVariable int id, Model model) {
         Optional<Cliente> cliente = service.listarId(id);
-
-        model.addAttribute("cliente", cliente);
+         model.addAttribute("cliente", cliente);
         return "ventaBoleto";
     }
 
     @GetMapping("/eliminar/{id}")
-    public String delete(@PathVariable int id){
+    public String delete(@PathVariable int id) {
         service.delete(id);
         return "redirect:/listar";
     }
-
 
 
 }
